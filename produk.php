ab269d63a63b85f96488db748e58d2185b54e66d
Dani170907 <daniramadhan170907@gmail.com>
2024-07-27 17:14:54 +0700

    melakukan commit

--- produk.php
+++ produk.php
@@ -1,10 +1,17 @@
 <?php
 
-// Jualan Produk
-// Komik
-// Game
 class Produk {
+    public $judul = "judul", 
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0;
 
 }
+
 $produk1 = new Produk();
+$produk1->judul = "Naruto";
+var_dump($produk1);
+
 $produk2 = new Produk();
+var_dump($produk2->judul);
+
