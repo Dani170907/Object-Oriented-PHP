a0370039158c368fd3a6d727a94ba53a23071f17
Dani170907 <daniramadhan170907@gmail.com>
2024-07-28 21:28:41 +0700

    add file object-type.php

---
+++ object-type.php
@@ -0,0 +1,41 @@
+<?php
+
+// Membuat Default Property
+class Produk {
+    // Mendefinisikan properti default dengan nilai awal
+    public $judul, 
+           $penulis,
+           $penerbit,
+           $harga;
+
+    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    // Membuat Method
+    // Method untuk mengembalikan label produk
+    public function getLabel() {
+        // Menggunakan properti objek saat ini
+        return "$this->judul, $this->penulis";
+    }
+}
+
+
+
+// Memanggil Object
+// Sebuah Instansiasi / Instance
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000); // Membuat objek baru dari class Produk
+
+// Mengubah properti objek $produk2
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$produk3 = new Produk("Need For Speed Most Wanted");
+// Menampilkan label produk untuk objek $produk3
+echo "Komik : " . $produk1->getLabel(); // Output: Komik : Naruto, Masashi Kishimoto
+echo "<br>";
+// Menampilkan label produk untuk objek $produk4
+echo "Game : " . $produk2->getLabel(); // Output: Game : Uncharted, Neil Druckmann
+echo "<br>";
+var_dump($produk3);
