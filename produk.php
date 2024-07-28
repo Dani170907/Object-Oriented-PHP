a56bc643dfd7f1bb2d41c57b7834df48671c854f
Dani170907 <daniramadhan170907@gmail.com>
2024-07-28 11:38:00 +0700

    update

--- produk.php
+++ produk.php
@@ -1,10 +1,16 @@
 <?php
 
+// Membuat Default Property
 class Produk {
     public $judul = "judul", 
         $penulis = "penulis",
         $penerbit = "penerbit",
         $harga = 0;
+
+    // Membuat Method
+    public function getLabel() {
+        return "$this->judul, $this->penulis";
+    }
 
 }
 
@@ -17,3 +23,19 @@ class Produk {
 // $produk2->tambahProperty = "Game Paling Keren";
 // var_dump($produk2);
 
+// Sebuah Instansiasi / Instance
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
