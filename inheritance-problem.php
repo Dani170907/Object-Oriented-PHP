093e7e9a1f7bc6ff69640a459511fb4fb9ff0603
Dani170907 <daniramadhan170907@gmail.com>
2024-07-29 16:32:14 +0700

    update file inheritance-problem.php

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -1,8 +1,7 @@
 <?php
 
-// Membuat Default Property
+
 class Produk {
-    // Mendefinisikan properti default dengan nilai awal
     public $judul, 
            $penulis,
            $penerbit,
@@ -15,10 +14,7 @@ class Produk {
         $this->harga = $harga;
     }
 
-    // Membuat Method
-    // Method untuk mengembalikan label produk
     public function getLabel() {
-        // Menggunakan properti objek saat ini
         return "$this->penulis, $this->penerbit";
     }
 }
@@ -30,18 +26,13 @@ class CetakInProduk {
     }
 }
 
-// Memanggil Object
-// Sebuah Instansiasi / Instance
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000); // Membuat objek baru dari class Produk
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 
-// Mengubah properti objek $produk2
 $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
 
-// Menampilkan label produk untuk objek $produk3
-echo "Komik : " . $produk1->getLabel(); // Output: Komik : Naruto, Masashi Kishimoto
+echo "Komik : " . $produk1->getLabel(); 
 echo "<br>";
-// Menampilkan label produk untuk objek $produk4
-echo "Game : " . $produk2->getLabel(); // Output: Game : Uncharted, Neil Druckmann
+echo "Game : " . $produk2->getLabel();
 echo "<br>";
 $infoProduk1 = new CetakInProduk();
 echo $infoProduk1->cetak($produk1);
