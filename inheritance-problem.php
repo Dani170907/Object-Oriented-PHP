4cf638ab79cd55e7bba4634c6d5d9a8201231002
Dani170907 <daniramadhan170907@gmail.com>
2024-07-29 16:33:13 +0700

    update file inheritance-problem.php

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -29,10 +29,3 @@ class CetakInProduk {
 $produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000);
 
 $produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
-
-echo "Komik : " . $produk1->getLabel(); 
-echo "<br>";
-echo "Game : " . $produk2->getLabel();
-echo "<br>";
-$infoProduk1 = new CetakInProduk();
-echo $infoProduk1->cetak($produk1);
