94466169e2f208b361e3075a0ede5c7ce117eb54
Dani170907 <daniramadhan170907@gmail.com>
2024-07-31 21:58:37 +0700

    update file static

--- static.php
+++ static.php
@@ -1,15 +1,27 @@
 <?php
 
-class ContohStatic {
-    public static $angka = 1;
+// class ContohStatic {
+//     public static $angka = 1;
 
-    public static function halo() {
-        return "Halo." . self::$angka++ . " kali.";
+//     public static function halo() {
+//         return "Halo." . self::$angka++ . " kali.";
+//     }
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+
+class Contoh {
+    public $angka = 1;
+    
+    public function halo() {
+        return "Halo" . $this->angka . "kali. <br>";
     }
 }
 
-echo ContohStatic::$angka;
-echo "<br>";
-echo ContohStatic::halo();
-echo "<hr>";
-echo ContohStatic::halo();
+$obj = new Contoh;
+echo $obj->halo();
