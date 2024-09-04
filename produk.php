cde822ba7c9e8dbb583028c70b99499112ffd817
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-04 17:32:23 +0700

    memberikan nilai default pada class Produk

--- produk.php
+++ produk.php
@@ -2,10 +2,14 @@
 
 class Produk {
     // Mendefinisikan property default dengan nilai awal
-    public $judul,
-    $penulis,
-    $penerbit,
-    $harga;
+    public $judul = "judul",
+    $penulis = "penulis",
+    $penerbit = "penerbit",
+    $harga = 0;
 }
+
+// Membuat instansiasi dari class produk
+$produk1 = new Produk();
+var_dump($produk1);
 
 ?>
