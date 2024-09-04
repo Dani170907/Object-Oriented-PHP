a285807efeeedb15d210af7e0d5ec8184b12a6a2
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-04 17:42:18 +0700

    mencoba melakukan intansiasi dan menimpa data default ketika sedang diinstansiasi

--- produk.php
+++ produk.php
@@ -3,19 +3,26 @@
 class Produk {
     // Mendefinisikan property default dengan nilai awal
     public $judul = "judul",
-    $penulis = "penulis",
-    $penerbit = "penerbit",
-    $harga = 0;
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
 }
 
-// Membuat instansiasi dari class produk
-$produk1 = new Produk();
-$produk1->judul = "Naruto";
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
 
-$produk2 = new Produk();
-$produk2 ->judul = "Uncharted";
-$produk2->tambahProperty = "property tambahan";
-var_dump($produk2);
+// $produk2 = new Produk();
+// $produk2 ->judul = "Uncharted";
+// $produk2->tambahProperty = "property tambahan";
+// var_dump($produk2);
+
+// Membuat instansiasi dari class produk
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 300000;
+var_dump($produk3);
 
 ?>
