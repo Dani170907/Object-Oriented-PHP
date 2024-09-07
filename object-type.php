53f3425c00c261c86a1ac740549cc68e364439d5
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-07 15:07:09 +0700

    memberikan komentar yang detail pada file boject type

--- object-type.php
+++ object-type.php
@@ -1,49 +1,56 @@
 <?php
 
-// Mendefinisikan class Produk untuk merepresentasikan produk
+// Mendefinisikan class Produk yang merepresentasikan produk
 class Produk {
     // Property yang akan diisi dengan nilai dari constructor
-    public $judul,
-           $penulis,
-           $penerbit,
-           $harga = 0;
+    public $judul, // Judul produk
+           $penulis, // Penulis produk
+           $penerbit, // Penerbit produk
+           $harga = 0; // Harga produk, nilai default 0
 
-    // Constructor untuk menerima parameter dan menginisialisasi property dari object
+    // Constructor untuk menerima parameter dan menginisialisasi property object
     // Jika tidak ada nilai yang diberikan, akan menggunakan nilai default
     public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0) {
         // Mengisi property class dengan nilai dari parameter
-        $this->judul = $judul;
-        $this->penulis = $penulis;
-        $this->penerbit = $penerbit;
-        $this->harga = $harga;
+        $this->judul = $judul; // Mengisi property 'judul'
+        $this->penulis = $penulis; // Mengisi property 'penulis'
+        $this->penerbit = $penerbit; // Mengisi property 'penerbit'
+        $this->harga = $harga; // Mengisi property 'harga'
     }
 
-    // Method untuk mengembalikan label produk (penulis dan penerbit)
+    // Method untuk mengembalikan label produk yang terdiri dari penulis dan penerbit
     public function getLabel() {
+        // Mengembalikan string dengan format "penulis, penerbit"
         return "$this->penulis, $this->penerbit";
     }
 }
 
+// Mendefinisikan class CetakInfoProduk untuk mencetak informasi produk
 class CetakInfoProduk {
-    public function cetak( Produk $produk) {
+    // Method cetak untuk mencetak detail produk
+    // Parameter $produk harus berupa instance dari class Produk
+    public function cetak( Produk $produk ) {
+        // Menggabungkan properti dari produk (judul, penulis, penerbit, dan harga) ke dalam satu string
         $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        // Mengembalikan string yang sudah terbentuk
         return $str;
     }
 }
 
-// Membuat object Produk dengan nilai yang diberikan ke constructor
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000); // Produk 1 adalah komik
-$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000); // Produk 2 adalah game
+// Membuat object Produk dengan mengirimkan nilai parameter ke constructor
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000); // Produk 1 adalah komik dengan informasi lengkap
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000); // Produk 2 adalah game dengan informasi lengkap
 
-// Menampilkan label produk 'Naruto'
+// Menampilkan label produk 'Naruto' menggunakan method getLabel
 echo "Komik : " . $produk1->getLabel();
-echo "<hr>"; // Garis pemisah dalam HTML
-// Menampilkan label produk 'Uncharted'
+echo "<hr>"; // Membuat garis horizontal di HTML
+// Menampilkan label produk 'Uncharted' menggunakan method getLabel
 echo "Game : " . $produk2->getLabel();
-echo "<hr>";
+echo "<hr>"; // Membuat garis horizontal di HTML
 
-// instansiasi class baru
+// Membuat instance dari class CetakInfoProduk untuk mencetak detail produk
 $infoProduk1 = new CetakInfoProduk();
+// Mencetak detail produk 'Naruto' menggunakan method cetak() dan menampilkan hasilnya
 echo $infoProduk1->cetak($produk1);
 
 ?>
