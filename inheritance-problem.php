755f897a9504252023c353879ff692e5f7f78e31
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-07 16:01:42 +0700

    menambahkan properti jmlHalaman, waktuMain dan tipe

--- inheritance-problem.php
+++ inheritance-problem.php
@@ -4,15 +4,19 @@ class Produk {
     public $judul, 
            $penulis,
            $penerbit,
-           $harga = 0;
-
+           $harga = 0,
+           $jmlHalaman,
+           $waktuMain,
+           $tipe;
     
-    
-    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0) {
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe ) {
         $this->judul = $judul; 
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga; 
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
+        $this->tipe = $tipe;
     }
 
     
@@ -29,8 +33,10 @@ class CetakInfoProduk {
     }
 }
 
-$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000); // Produk 1 adalah komik dengan informasi lengkap
-$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000); // Produk 2 adalah game dengan informasi lengkap
+$produk1 = new Produk("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik"); 
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
 
+// Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman
+// Game : Uncharted | Neil Druckmann, Sony Computer (Rp. 250000) ~ 5- Jam
 
 ?>
