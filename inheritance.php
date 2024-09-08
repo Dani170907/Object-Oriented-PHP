fdc665daa7a71369f31e7943dbdef84ae0419a58
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-08 18:00:39 +0700

    membuat child class Game dengan parent class Produk dan method baru getInfoProduk

--- inheritance.php
+++ inheritance.php
@@ -6,17 +6,15 @@ class Produk {
            $penerbit,
            $harga = 0,
            $jmlHalaman,
-           $waktuMain,
-           $tipe;
+           $waktuMain;
     
-    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0, $tipe ) {
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0 ) {
         $this->judul = $judul; 
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga; 
         $this->jmlHalaman = $jmlHalaman;
         $this->waktuMain = $waktuMain;
-        $this->tipe = $tipe;
     }
 
     public function getLabel() {
@@ -24,14 +22,7 @@ class Produk {
     }
 
     public function getInfoProduk() {
-        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ";
-
-        if ($this->tipe == "Komik") {
-            $str .= " - {$this->jmlHalaman} Halaman.";
-        } elseif ($this->tipe == "Game") {
-            $str .= " ~ {$this->waktuMain} Jam.";
-        }
-
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ";
         return $str;
     }
 }
@@ -47,13 +38,24 @@ class CetakInfoProduk {
 class Komik extends Produk {
     public function getInfoProduk()
     {
-        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman";
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+        return $str;
     }
 }
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0, "Komik"); 
-$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50, "Game");
+class Game extends Produk {
+    public function getInfoProduk()
+    {
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0); 
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50);
 
 echo $produk1->getInfoProduk();
+echo "<hr>";
+echo $produk2->getInfoProduk();
 
 ?>
