2108d247e5819ab7e5a9c89e72f6b8708c29fac0
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-11 20:08:22 +0700

    memanggil method dari class parent ke child class menggunakan sintaks parent::getInfoProduk

--- overriding.php
+++ overriding.php
@@ -38,7 +38,7 @@ class CetakInfoProduk {
 class Komik extends Produk {
     public function getInfoProduk()
     {
-        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+        $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
         return $str;
     }
 }
@@ -46,7 +46,7 @@ class Komik extends Produk {
 class Game extends Produk {
     public function getInfoProduk()
     {
-        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->waktuMain} Jam.";
+        $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
         return $str;
     }
 }
