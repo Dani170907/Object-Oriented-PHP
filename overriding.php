6cf20beee9412f1c2863d8a3b4797b1967a3df78
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-11 20:22:28 +0700

    memanggil construct dari parent class ke child class untuk men-general property

--- overriding.php
+++ overriding.php
@@ -4,17 +4,13 @@ class Produk {
     public $judul, 
            $penulis,
            $penerbit,
-           $harga = 0,
-           $jmlHalaman,
-           $waktuMain;
+           $harga = 0;
     
-    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0 ) {
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0 ) {
         $this->judul = $judul; 
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
-        $this->harga = $harga; 
-        $this->jmlHalaman = $jmlHalaman;
-        $this->waktuMain = $waktuMain;
+        $this->harga = $harga;
     }
 
     public function getLabel() {
@@ -36,6 +32,14 @@ class CetakInfoProduk {
 }
 
 class Komik extends Produk {
+    public $jmlHalaman;
+
+    public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0 ) {
+        parent::__construct( $judul, $penulis, $penerbit, $harga );
+
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
     public function getInfoProduk()
     {
         $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
@@ -44,6 +48,14 @@ class Komik extends Produk {
 }
 
 class Game extends Produk {
+    public $waktuMain;
+
+    public function __construct( $judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $waktuMain = 0 ) {
+        parent::__construct( $judul, $penulis, $penerbit, $harga );
+
+        $this->waktuMain = $waktuMain;
+    }
+
     public function getInfoProduk()
     {
         $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
@@ -51,8 +63,8 @@ class Game extends Produk {
     }
 }
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0); 
-$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50);
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100); 
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 
 echo $produk1->getInfoProduk();
 echo "<hr>";
