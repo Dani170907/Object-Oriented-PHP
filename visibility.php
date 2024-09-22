b0c1814e88d04ba778752fb59c4f28a53e64423e
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-22 11:47:18 +0700

    mulai ulang file visibility.php

--- visibility.php
+++ visibility.php
@@ -1,95 +1,61 @@
 <?php
 
-//Parent Class
 class Produk {
     public $judul, 
            $penulis,
-           $penerbit;
-
-    protected $diskon = 0;
-
-    private $harga;
-
+           $penerbit,
+           $harga = 0,
+           $jmlHalaman,
+           $waktuMain;
     
-    public function __construct( 
-        $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", 
-        $harga = 0
-        ) {
-            $this->judul = $judul;
-            $this->penulis = $penulis;
-            $this->penerbit = $penerbit;
-            $this->harga = $harga;
-        }
-        
-
-    public function getHarga() {
-        return $this->harga - ( $this->harga * $this->diskon / 100 );
+    public function __construct($judul = "Judul", $penulis = "Penulis", $penerbit = "Penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0 ) {
+        $this->judul = $judul; 
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga; 
+        $this->jmlHalaman = $jmlHalaman;
+        $this->waktuMain = $waktuMain;
     }
-        
+
     public function getLabel() {
         return "$this->penulis, $this->penerbit";
     }
-    public function getInfoProduk() {
-        // Komik : Naruto | masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman
-        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
 
+    public function getInfoProduk() {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ";
         return $str;
     }
 }
-// end Parent Class
 
-// Child Class
-class Komik extends Produk {
-    public $jmlHalaman;
+class CetakInfoProduk {
 
-    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", 
-    $harga = 0, $jmlHalaman = 0 ) {
-        parent::__construct( $judul, $penulis, $penerbit, $harga );
-    
-        $this->jmlHalaman = $jmlHalaman;
+    public function cetak( Produk $produk ) {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
     }
+}
 
-    public function getInfoProduk() {
-        $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+class Komik extends Produk {
+    public function getInfoProduk()
+    {
+        $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
         return $str;
     }
 }
 
 class Game extends Produk {
-    public $waktuMain;
-
-    public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", 
-    $harga = 0, $waktuMain = 0 ) {
-        parent::__construct( $judul, $penulis, $penerbit, $harga);
-        $this->waktuMain = $waktuMain;
-    }
-
-    public function setDiskon( $diskon ) {
-        return $this->diskon = $diskon;
-    }
-
-    public function getInfoProduk() {
-        $str = "Game : " . parent::getInfoProduk() ." ~ {$this->waktuMain} Jam.";
-        return $str;
-    }
-}
-// end Child Class
-
-class CetakInfoProduk {
-    public function cetak( Produk $produk ) {
-        $str = "{$produk->judul} | {$produk->getLabel()}, (Rp. {$produk->harga})";
+    public function getInfoProduk()
+    {
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) ~ {$this->waktuMain} Jam.";
         return $str;
     }
 }
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
-$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0); 
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50);
 
 echo $produk1->getInfoProduk();
-echo '<br>';
-echo $produk2->getInfoProduk();
 echo "<hr>";
+echo $produk2->getInfoProduk();
 
-$produk2->setDiskon(50);
-
-echo $produk2->getHarga();
+?>
