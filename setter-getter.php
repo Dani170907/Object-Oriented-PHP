a73c8572e60b7c394e4c1ab5c8f756722ed3eadb
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-24 07:41:17 +0700

    menambahkan method setter dan getter untuk penulis dan penerbit

--- setter-getter.php
+++ setter-getter.php
@@ -24,6 +24,22 @@ class Produk {
         return $this->judul; 
     }
 
+    public function setPenulis( $penulis ) {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis() {
+        return $this->penulis;
+    }
+
+    public function setPenerbit( $penerbit ) {
+        $this->penerbit = $penerbit;
+    }
+    
+    public function getPenerbit() {
+        return $this->penerbit;
+    }
+
     public function getHarga() {
         return $this->harga - ( $this->harga * $this->diskon / 100 );
     }
