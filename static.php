ab774c09d1b54a41e50edc0ee2a2a69920bc5189
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-27 21:08:06 +0700

    mengaktifkan kode

--- static.php
+++ static.php
@@ -1,37 +1,38 @@
 <?php
 
-// class ContohStatic {
-//     public static $angka = 1;
-
-//     public static function halo() {
-//         return "Halo. " . self::$angka++ . " Kali";
-//     }
-// }
-
-// echo ContohStatic::$angka;
-// echo "<br>";
-// echo ContohStatic::halo();
-// echo "<hr>";
-// echo ContohStatic::halo();
-
-class Contoh {
+class ContohStatic {
     public static $angka = 1;
 
-    public function halo() {
-        return "Halo. " . self::$angka++ . " Kali<br>";
+    public static function halo() {
+        return "Halo. " . self::$angka++ . " Kali";
+    }
+}
+
+echo ContohStatic::$angka;
+echo "<br>";
+echo ContohStatic::halo();
+echo "<br>";    
+echo ContohStatic::halo();
+echo "<hr>";
+
+class Contoh {
+    public static $nomor = 1;
+
+    public function hai() {
+        return "Hai. " . self::$nomor++ . " Kali<br>";
     }
 }
 
 $obj = new Contoh;
-echo $obj->halo();
-echo $obj->halo();
-echo $obj->halo();
+echo $obj->hai();
+echo $obj->hai();
+echo $obj->hai();
 
 echo "<br>";
 
 $obj2 = new Contoh();
-echo $obj2->halo();
-echo $obj2->halo();
-echo $obj2->halo();
+echo $obj2->hai();
+echo $obj2->hai();
+echo $obj2->hai();
 
 ?>
