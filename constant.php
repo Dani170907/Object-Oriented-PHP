773c8c37352d47eb65d68caa63b13ec88a33dc14
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-27 21:39:54 +0700

    mencoba membuat constanta

---
+++ constant.php
@@ -0,0 +1,17 @@
+<?php
+
+// define('NAMA', 'Dani Ramadhan'); 
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 17;
+// echo UMUR;
+
+class Coba {
+    const NAMA = 'Dani';
+}
+
+echo Coba::NAMA;
+
+?>
