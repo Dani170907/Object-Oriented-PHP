801afeef426e171211bfca3a92ae3bf04efd7d94
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-27 20:23:15 +0700

    membuat class static

---
+++ static.php
@@ -0,0 +1,15 @@
+<?php
+
+class ContohStatic {
+    public static $angka = 1;
+
+    public static function halo() {
+        return "Halo.";
+    }
+}
+
+echo ContohStatic::$angka;
+echo "<br>";
+echo ContohStatic::halo();
+
+?>
