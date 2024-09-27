ba28ddd830138072f9e9614cd21507e23b5027da
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-27 20:41:18 +0700

    membuat static keyword menggunakan instansiasi

--- static.php
+++ static.php
@@ -1,15 +1,37 @@
 <?php
 
-class ContohStatic {
-    public static $angka = 1;
+// class ContohStatic {
+//     public static $angka = 1;
 
-    public static function halo() {
-        return "Halo." . self::$angka . "Kali";
+//     public static function halo() {
+//         return "Halo. " . self::$angka++ . " Kali";
+//     }
+// }
+
+// echo ContohStatic::$angka;
+// echo "<br>";
+// echo ContohStatic::halo();
+// echo "<hr>";
+// echo ContohStatic::halo();
+
+class Contoh {
+    public $angka = 1;
+
+    public function halo() {
+        return "Halo. " . $this->angka++ . " Kali<br>";
     }
 }
 
-echo ContohStatic::$angka;
+$obj = new Contoh;
+echo $obj->halo();
+echo $obj->halo();
+echo $obj->halo();
+
 echo "<br>";
-echo ContohStatic::halo();
+
+$obj2 = new Contoh();
+echo $obj2->halo();
+echo $obj2->halo();
+echo $obj2->halo();
 
 ?>
