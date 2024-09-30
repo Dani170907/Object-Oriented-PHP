d9d94d5b597d95ba45b6c8d0bedf4ac719bd308f
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-09-30 20:15:55 +0700

    memanggil file-file yang berisi class

--- autoloading/index.php
+++ autoloading/index.php
@@ -1,5 +1,11 @@
 <?php
 
+require_once "App/Produk/InfoProduk.php";
+require_once "App/Produk/Produk.php";
+require_once "App/Produk/Komik.php";
+require_once "App/Produk/Game.php";
+require_once "App/Produk/CetakInfoProduk.php";
+
 $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100); 
 $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 
