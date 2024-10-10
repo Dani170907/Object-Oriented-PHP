6e29ee44c165f5e7565255ecf3bbac452becbb2b
Dani Ramadhan <daniramadhan170907@gmail.com>
2024-10-10 16:19:53 +0700

    penambahan namespace pada setiap class yang memiliki nama yang sama pada vendor yang berbeda

---
+++ namespace/App/init.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "Produk/InfoProduk.php";
+require_once "Produk/Produk.php";
+require_once "Produk/Komik.php";
+require_once "Produk/Game.php";
+require_once "Produk/CetakInfoProduk.php";
+
+require_once "Service/User.php";
+
+// spl_autoload_register(function( $class ) {
+//     require_once __DIR__ . '/Produk/' . $class . '.php';
+// });
